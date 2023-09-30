4171bd0523c0c8530f4674ba8975ae5cea535423
Shanmugam37 <99902963+Shanmugam37@users.noreply.github.com>
2023-09-30 16:43:26 +0530

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,28 +1,28 @@
 
-#bucket creation 
-resource "aws_s3_bucket" "google_s3_bucket" {
-  bucket = "this_bucket_is_used_by_google"
-  tags = {
-    Name        = "google_bucket"
-    environment = "Development"
-  }
-}
+# #bucket creation 
+# resource "aws_s3_bucket" "google_s3_bucket" {
+#   bucket = "this_bucket_is_used_by_google"
+#   tags = {
+#     Name        = "google_bucket"
+#     environment = "Development"
+#   }
+# }
 
-#  acl control
-resource "aws_s3_bucket_public_access_block" "access_block" {
-  bucket                  = aws_s3_bucket.google_s3_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# #  acl control
+# resource "aws_s3_bucket_public_access_block" "access_block" {
+#   bucket                  = aws_s3_bucket.google_s3_bucket.id
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-#### versioning "enable"
+# #### versioning "enable"
 
-resource "aws_s3_bucket_versioning" "s3_versioning" {
-  bucket = aws_s3_bucket.google_s3_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+# resource "aws_s3_bucket_versioning" "s3_versioning" {
+#   bucket = aws_s3_bucket.google_s3_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
 
-}
+# }
